3d9557f69c39dd280a51a238feeac46a223db863
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-06 18:45:38 +0530

    Add files via upload

---
+++ KrishnamurthyNumber.java
@@ -0,0 +1,36 @@
+// factorial sum of all it's digits = number itself
+import java.util.*;
+import java.io.*;
+
+public class KrishnamurthyNumber {
+    public static void main(String[] args) {
+        int range;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number : ");
+        range = sc.nextInt();
+        for(int i=1; i<=range; i++){
+            checkNumber(i);
+        }
+    }
+    static int fact(int num){
+        int f = 1;
+        while(num!=0){
+            f*=num;
+            num--;
+        }
+        return f;
+    }
+
+    static void checkNumber(int num){
+        int sum = 0;
+        int tempNumber = num;
+
+        while(tempNumber!=0){
+            sum+= fact(tempNumber%10);
+            tempNumber /= 10;
+        }
+        if(sum==num){
+            System.out.println(num+" is a krishnamurthy number");
+        }
+    }
+}
