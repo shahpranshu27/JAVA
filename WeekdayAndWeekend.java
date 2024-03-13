24a0c6ddcadf13bb495d068adbda10e86d51c7d0
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-13 15:40:14 +0530

    Update WeekdayAndWeekend.java

--- WeekdayAndWeekend.java
+++ WeekdayAndWeekend.java
@@ -6,16 +6,28 @@ public class WeekdayAndWeekend {
         System.out.println("Enter a number to check if it's weekday or weekend: ");
         int n = sc.nextInt();
 
+        // switch (n) {
+        //     case 1:
+        //     case 2:
+        //     case 3:
+        //     case 4:
+        //     case 5:
+        //         System.out.println("Weekday!! Himmat rakho");
+        //         break;
+        //     case 6:
+        //     case 7:
+        //         System.out.println("Weekend!! Chill time");
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+        // above switch case can also be written in this way. Short and sweet! 
         switch (n) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
+            case 1,2,3,4,5:
                 System.out.println("Weekday!! Himmat rakho");
                 break;
-            case 6:
-            case 7:
+            case 6,7:
                 System.out.println("Weekend!! Chill time");
                 break;
             default:
