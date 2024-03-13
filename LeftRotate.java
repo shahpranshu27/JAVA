c3c1e8d2a1cd32a6abc4e1026661300af6131d12
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-13 16:28:43 +0530

    Create LeftRotate.java

---
+++ LeftRotate.java
@@ -0,0 +1,23 @@
+public class LeftRotate {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5};
+        int n = 3; // n-> number of times array should be left rotated
+        System.out.println("Original array : ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        for(int i=0; i<n; i++){
+            int j,first;
+            first = arr[0];
+            for(j=0; j<arr.length-1; j++){
+                arr[j] = arr[j+1];
+            }
+            arr[j] = first;
+        }
+        System.out.println();
+        System.out.println("Array after left rotating: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
