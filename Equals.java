26f97eb720887d612b54707db84ad84653867833
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-13 15:16:49 +0530

    Create Equals.java

---
+++ Equals.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Equals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of fruit: ");
+        String fruit = sc.next();
+
+        if(fruit.equals("mango")){
+            System.out.println("King of fruits");
+        }
+        else if(fruit.equals("apple")){
+            System.out.println("An apple a day, keeps the doctor away!");
+        }
+        else{
+            System.out.println("enter the name of fruit!!");
+        }
+    }
+}
