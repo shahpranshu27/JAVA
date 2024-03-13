c0eec5bb9e1d815a7a87ae2cd753573709c13c85
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-13 15:10:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I am Pranshu, an SDE intern at Rays TechServ");
+        System.out.println("I am making this program, as a part of my learning java, which i am doing for company as well as university purpose");
     }
 }
