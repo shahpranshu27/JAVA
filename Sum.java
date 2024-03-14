05d89cef7697cfc98f2e49a5ca15b0442341f705
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-14 11:45:28 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        // sum();
+        System.out.println("Sum of 2 numbers : "+sum());
     }
-    static void sum(){
+    static int sum(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter num1: ");
         int num1 = sc.nextInt();
         System.out.println("Enter num2: ");
         int num2 = sc.nextInt();
-        System.out.println("Sum of 2 numbers: "+(num1+num2));
+        // System.out.println("Sum of 2 numbers: "+(num1+num2)); // if return type is void
         sc.close();
+        return (num1+num2); // only if return type is int or float or double or something, then only we use return type 
     }
 }
