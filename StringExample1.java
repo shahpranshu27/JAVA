67451bf7814ff3b412c7aa40e87f3ee79c7342d9
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-14 12:11:37 +0530

    Update StringExample1.java

--- StringExample1.java
+++ StringExample1.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class StringExample1 {
     public static void main(String[] args) {
-        System.out.println(myGreet("Pranshu Shah"));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println(myGreet(name)); // if want to take name through user
+        System.out.println(myGreet("Pranshu shah")); // if directly want to pass the name as an arguement
+        sc.close();
     }
 
     static String myGreet(String name){
