406a765dbfdf9c63aac085b19a1b4c8d0b3a4ef9
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-14 12:29:10 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,10 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a=10, b=20, temp;
+        System.out.println(a+" "+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a+" "+b);
+    }
+}
