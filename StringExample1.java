cd587061b708ac2136cbae0196a067ab00dd9a71
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-14 12:08:14 +0530

    Create StringExample1.java

---
+++ StringExample1.java
@@ -0,0 +1,9 @@
+public class StringExample1 {
+    public static void main(String[] args) {
+        System.out.println(myGreet("Pranshu Shah"));
+    }
+
+    static String myGreet(String name){
+        return "Hello "+ name;
+    }
+}
