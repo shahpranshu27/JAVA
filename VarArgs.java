d5410584faf024170d97afffe1b38e765a928662
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-14 15:31:08 +0530

    Update VarArgs.java

--- VarArgs.java
+++ VarArgs.java
@@ -2,11 +2,17 @@ import java.util.Arrays;
 
 public class VarArgs {
     public static void main(String[] args) {
-        fun(1,2,3,4,5,6,7,8,9);
+        // fun(1,2,3,4,5,6,7,8,9);
+        multiple(23, 540, "Pranshu", "Yashvi", "Siddh", "Siddhi");
     }
-
+    // Note: variable length arguements should always come at the end
     static void fun(int ...v){ // ...any_char will create an array of integers 
         System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String ...s){
+        System.out.print(a+" "+b+" "+Arrays.toString(s));
+        // System.out.println(Arrays.toString(s));
     }
     // for Strings array -> ...anything
     // for char array -> ...anything 
