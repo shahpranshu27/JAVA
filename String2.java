2e4a32cd7f573bf72bc5252825ebbfa9bf393eab
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-15 12:25:14 +0530

    Create String2.java

---
+++ String2.java
@@ -0,0 +1,7 @@
+public class String2 {
+    public static void main(String[] args) {
+        String name = "Pranshu";
+        name = name.concat(" Shah");
+        System.out.println("name: "+name);
+    }
+}
