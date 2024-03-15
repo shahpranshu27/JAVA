a0618b9110b6568b47d1fa59882627461fc0f174
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-15 15:10:04 +0530

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -29,12 +29,6 @@ public class Array2 {
             System.out.println(Arrays.toString(arr[i]));
         }
 
-        // int[][] arr1 = {
-        //     {1, 2, 3},
-        //     {4, 5},
-        //     {7, 8, 9, 10}
-        // };
-
         // for(int i=0; i<arr1.length; i++){
         //     for(int j=0; j<arr1[i].length; j++){
         //         System.out.print(arr1[i][j]+" ");
