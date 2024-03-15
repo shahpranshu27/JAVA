445355a435ac057198d7af2470cf269eaabef1e5
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-15 15:33:48 +0530

    Create Array4.java

---
+++ Array4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        Scanner sc = new Scanner(System.in);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        System.out.println(list);
+        list.set(0, 11);
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+
+        for(int i=0; i<5; i++){
+            list.add(sc.nextInt());
+        }
+
+        System.out.println(list);
+    }
+}
