3058912ad81a9f624a723f82da250c05e44f57cd
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-15 14:50:11 +0530

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 
 public class Array2 {
     public static void main(String[] args) {
-        int[][] arr = new int[3][3];
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        int r = sc.nextInt();
+        System.out.println("Enter number of cols: ");
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
 
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
