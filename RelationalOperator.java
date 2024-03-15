741ef6cfccfe5d2501fe7a89aaf94ae5842642dc
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-15 11:23:32 +0530

    Create RelationalOperator.java

---
+++ RelationalOperator.java
@@ -0,0 +1,26 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int n1=10, n2=10, n3=20;
+        System.out.println("n1: "+n1);
+        System.out.println("n2: "+n2);
+        System.out.println("n3: "+n2);
+
+        // == operator 
+        System.out.println("n1 == n2: "+(n1 == n2));
+        System.out.println("n2 == n3: "+(n2 == n3));
+
+        // != operator
+        System.out.println("n1 != n2: "+(n1 != n2));
+        System.out.println("n2 != n3: "+(n2 != n3));
+
+        // > & < operator
+        System.out.println("n1 > n2: "+(n1 > n2));
+        System.out.println("n2 < n3: "+(n2 > n3));
+
+        // >= & <= operator
+        System.out.println("n1 >= n2: "+(n1 >= n2));
+        System.out.println("n2 <= n3: "+(n2 <= n3));
+
+        
+    }
+}
