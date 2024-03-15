71d918e2b13e76491304994a533f432b7c6c1a7b
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-15 11:07:29 +0530

    Create ArithmeticOperator.java

---
+++ ArithmeticOperator.java
@@ -0,0 +1,19 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int n1=110, n2=20;
+        int sum = n1+n2; // addition
+        System.out.println("Sum: "+sum);
+
+        int diff = n1-n2; // difference or subtraction
+        System.out.println("Difference: "+diff);
+
+        int mul = n1*n2; // multiplication
+        System.out.println("Product: "+mul);
+
+        int div = n1/n2; // gives quotient
+        System.out.println("Division: "+div);
+
+        int mod = n1%n2; // gives remainder after n1/n2
+        System.out.println("Mod: "+mod);
+    }
+}
