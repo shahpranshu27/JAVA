9b60a33405e5ee9f4a155f4e0d3fcddc57aa8fe1
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-18 11:10:31 +0530

    Update ArrayDescending.java

--- ArrayDescending.java
+++ ArrayDescending.java
@@ -7,6 +7,7 @@ public class ArrayDescending {
             System.out.print(arr[i]+" ");
         }
 
+        // sort in descending order
         for (int i = 0; i < arr.length; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 if(arr[j]>arr[i]){
