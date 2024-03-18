e6ac61af5d4b74048f04aae2e192789c65853c58
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-18 11:10:09 +0530

    Create ArrayDescending.java

---
+++ ArrayDescending.java
@@ -0,0 +1,25 @@
+public class ArrayDescending {
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,5,1};
+        int temp=0;
+        System.out.println("Original array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j]>arr[i]){
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nSorted array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
