f1c46644adfad3cb5cdb673b3dd569836a939b8e
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-18 11:05:13 +0530

    Create ArrayAscending.java

---
+++ ArrayAscending.java
@@ -0,0 +1,25 @@
+public class ArrayAscending {
+    public static void main(String[] args) {
+        int[] arr = {4,2,5,3,1};
+        int temp = 0;
+        System.out.println("Original array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        
+        // sort in ascending order
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("\nSorted array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
