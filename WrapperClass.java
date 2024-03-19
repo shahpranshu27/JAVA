5582343f5ae6b8186ad2e4bed1da3034c38d7dd4
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-19 14:36:09 +0530

    Create WrapperClass.java

---
+++ WrapperClass.java
@@ -0,0 +1,26 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        swap(a, b);
+        System.out.println(a+" "+b);
+        // wrapper class i.e. now, num = 12 is an object  
+        Integer num = 12;
+        Integer num1 = 14;
+        swap(num, num1); // still won't swap because Integer is a 'final' class, so it's value once assigned, can't be changed
+        System.out.println(num +" "+ num1);
+    }
+
+    static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void swap(Integer a, Integer b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
