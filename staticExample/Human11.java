d9a04fb4aea98df5ebd440a45f302e36d50a3461
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-20 15:52:26 +0530

    Update Human11.java

--- staticExample/Human11.java
+++ staticExample/Human11.java
@@ -1,4 +1,7 @@
 package staticExample;
+
+import hello.bye.good.night.hello;
+
 // import staticExample.Human;
 public class Human11 {
     public static void main(String[] args) {
@@ -18,7 +21,31 @@ public class Human11 {
         System.out.println(Human.population);
         System.out.println(Human.population);
         System.out.println(Human.population);
+        hello();
+        greet();
         // Remember: static variables are not dependent on objects
     }
 
+    // remember, we can not use non static method inside the static method. that is why, we can not use any non static method inside the main method  
+    static void hello(){
+        // System.out.println("heluu");
+        // greeting();// will give error
+        greet(); // this won't give error, as greet() is a static method, so we can use it in static method
+    }
+
+    static void greet(){
+        System.out.println("Hello");
+    }
+
+    void greeting(){
+        System.out.println("hi");
+        message(); // this also won't give error, as we can declare or use non static method in the non static method
+    }
+
+    void message(){
+        System.out.println("message");
+        greeting();
+        greet(); // we can use static methods inside the non static methods, but vice versa isn't true 
+        hello();
+    }
 }
