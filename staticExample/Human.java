5a78132b3bc327fa5430264926779d022379b459
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-20 15:31:29 +0530

    Update Human.java

--- staticExample/Human.java
+++ staticExample/Human.java
@@ -13,5 +13,6 @@ public class Human {
         this.salary = salary;
         this.married = married;
         Human.population += 1; // here, even if we put this.population += 1; it will increase the value by 1 each and everytime new Human is created/instantiated, since it's static now, the new value will be stored everytime.
+        // here, since we are using Human.population, it counts how many new Human examples are being created, and returns that value
     }
 }
