72e4ddcbbf6c16ba1f8b96c6d50218509c38b698
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-20 12:13:12 +0530

    use case of packages

--- b/Sum1.java
+++ b/Sum1.java
@@ -4,4 +4,7 @@ public class Sum1 {
     public static void main(String[] args) {
         System.out.println("Sum1 in b folder");
     }
+    // the simple use case of package can be explained as, it is useful in managing the same named java files in different folders, who have different uses.
+    // like for eg. sum1.java in b folder has different code, sum1.java in hello folder has different code/use, sum1.java in hello.bye folder has different use and so on. 
+    // so, even though they have different uses, but same names, we can manage them by making different packages/folders
 }
