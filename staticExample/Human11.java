378bf2c4ec001d6bc207a1049d020de0a2e57d6d
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-20 14:57:12 +0530

    testing packages

---
+++ staticExample/Human11.java
@@ -0,0 +1,10 @@
+package staticExample;
+// import staticExample.Human;
+public class Human11 {
+    public static void main(String[] args) {
+        Human pranshu = new Human(21,"Pranshu",0, false);
+        System.out.println(pranshu.age);
+        System.out.println(pranshu);
+    }
+
+}
