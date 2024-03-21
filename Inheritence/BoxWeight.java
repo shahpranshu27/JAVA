a652e03e6280bf5c83deab437a67e4fa551dbc20
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-21 15:35:00 +0530

    Update BoxWeight.java

--- Inheritence/BoxWeight.java
+++ Inheritence/BoxWeight.java
@@ -6,4 +6,12 @@ public class BoxWeight extends Box {
     public BoxWeight(){
         this.weight = -1;
     }
+
+    public BoxWeight(double l, double w, double h, double weight){
+        super(l, w, h); // what is super()? it calls the parent class constructor i.e. in this case, it calls and takes values of l, w, h from the Box.java and takes weight from here
+        // used to initialize the values present in parent class
+        // if the parent class is declared public, then only it will be able to use the super() class, else, we won't be able to use it
+        this.weight = weight;
+
+    }
 }
