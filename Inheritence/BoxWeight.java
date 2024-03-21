e7bfc2370625f6114015901cd31c065fbfc2818d
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-21 17:02:05 +0530

    created some methods

--- Inheritence/BoxWeight.java
+++ Inheritence/BoxWeight.java
@@ -7,6 +7,20 @@ public class BoxWeight extends Box {
         this.weight = -1;
     }
 
+    BoxWeight(double weight){
+        this.weight = weight;
+    }
+
+    BoxWeight (BoxWeight other){
+        super(other); // here, it is not giving any error, even though in the Box.java file, we have written that it accepts only Box type as a parameter. so this is just same as we did earlier, BoxWeight class has access to Box class, so we can pass it, but we won't be able to pass Box class in the BoxWeight parameter
+        weight = other.weight;
+    }
+
+    BoxWeight(double side, double weight){
+        super(side);
+        this.weight = weight;
+    }
+
     public BoxWeight(double l, double w, double h, double weight){
         super(l, w, h); // what is super()? it calls the parent class constructor i.e. in this case, it calls and takes values of l, w, h from the Box.java and takes weight from here
         // used to initialize the values present in parent class
