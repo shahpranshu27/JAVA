1ac421849268f39899f7320de11c0bda2965eb32
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-21 12:46:27 +0530

    Update InnerClass2.java

--- staticExample/InnerClass2.java
+++ staticExample/InnerClass2.java
@@ -11,6 +11,10 @@ public class InnerClass2 {
 
     // Static nested classes allow you to create separate instances with their own variables, unlike static variables that share a single value across all instances.
 
+    // Instances are concrete objects created from a class.
+    // Each instance has its own set of instance variables to store its own data.
+    // Static nested classes allow you to create instances even without an instance of the outer class.
+    // Instance variables, unlike static variables, have separate values for each instance.
     public static void main(String[] args) {
         Test3 a3 = new Test3("a3");
         Test3 b3 = new Test3("b3");
