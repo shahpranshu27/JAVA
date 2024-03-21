103fd136bff12da56abf3ef537b571b6de2ac431
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-21 18:25:02 +0530

    Create Numbers.java

---
+++ Polymorphism/Numbers.java
@@ -0,0 +1,24 @@
+package Polymorphism;
+
+public class Numbers {
+    double sum(double a, int b){
+        return a+b; 
+    }
+
+    double sum(int a, int b){
+        return a+b;
+    }
+
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+        Numbers n1 = new Numbers();
+        System.out.println("sum1: "+n1.sum(2,3)); // even though a is input as int, it will be automatically typecasted into double
+        System.out.println("sum2: "+n1.sum(2,3,4));
+        // System.out.println("sum2: "+n1.sum(2,3,4, 5)); // this will give an error as there is no sum method which takes 4 arguements
+
+        System.out.println("sum1: "+n1.sum(2.34,3));
+    }
+}
