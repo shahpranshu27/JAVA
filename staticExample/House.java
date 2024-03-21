b2f930e4defbaac9866f7190e4f6590bb04a9778
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-21 12:50:32 +0530

    quite important update

--- staticExample/House.java
+++ staticExample/House.java
@@ -46,3 +46,28 @@ public class House {
         System.out.println(h3.name + " "+ h3.area +" "+ h3.city);
     }
 }
+
+// Package (staticExample):
+// This line defines the package name for your code. It helps organize your classes and avoid naming conflicts with classes from other projects.
+
+// Class (Home):
+// This is a blueprint (class) for defining houses.
+// It has three instance variables: name, area, and city. These variables will store information specific to each house (instance).
+// The constructor Home(String name, String area, String city) allows you to create new houses by providing their details during creation.
+
+// Instances (h1, h2, h3):
+// In the main method, you create three instances of the Home class:
+// Home h1 = new Home("Shikhar", "Gurukul", "Ahmedabad");: This creates a house named "Shikhar" in the "Gurukul" area of "Ahmedabad". h1 is the instance name for this specific house.
+// You create two more houses (h2 and h3) with their own details using the constructor.
+
+// Instance Variables and Printing:
+// System.out.println(h1.name + " " + h1.area + " " + h1.city);: This line accesses the instance variables (name, area, and city) of the h1 instance and prints their values ("Shikhar", "Gurukul", and "Ahmedabad").
+// Similarly, the following lines print the details of h2 and h3 using their respective instance variables.
+
+// Key Points Illustrated:
+// The Home class acts as a blueprint for creating individual houses (instances).
+// Each instance (h1, h2, h3) has its own copy of instance variables, allowing them to store unique information.
+// You can create multiple instances from the same class, representing different houses with their own details.
+
+// Non-Static Members:
+// In this example, all the members of the Home class (constructor, instance variables) are non-static. This means they are specific to each instance and require an object (instance) to be accessed.
