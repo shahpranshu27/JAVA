4e525168945848b2fdd6a4c2e7912bf9530b84ce
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-22 16:37:26 +0530

    Update SubClass.java

--- Access/SubClass.java
+++ Access/SubClass.java
@@ -9,5 +9,8 @@ public class SubClass extends A {
     public static void main(String[] args) {
         SubClass obj = new SubClass(122," null");
         System.out.println(obj.num);
+
+        System.out.println(obj instanceof SubClass);
+        System.out.println(obj instanceof A);
     }
 }
