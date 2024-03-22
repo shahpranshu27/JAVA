a0599d2ed25dd1b384ca0b49e618e61e0c5f598b
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-22 11:53:43 +0530

    Update Circle.java

--- Polymorphism/Circle.java
+++ Polymorphism/Circle.java
@@ -1,7 +1,13 @@
 package Polymorphism;
 
 public class Circle extends Shapes{
+    // this will run when object of circle is created, hence it is overriding the parent method
+    @Override // this is called annotation
     void area(){
         System.out.println("I am in circle!");
+    }
+
+    void area1(){
+        System.out.println("Am i in circle??");
     }
 }
