37f179edf213119936048b06df7044df55510146
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 15:32:20 +0530

    Update Main.java

--- interfacs/Main.java
+++ interfacs/Main.java
@@ -2,23 +2,32 @@ package interfacs;
 
 public class Main {
     public static void main(String[] args) {
-        // Car car1 = new Car();
+        Car car1 = new Car();
         // // Engine car1 = new Car();
 
         // // car1.a;
         // car1.acc();
         // car1.start();
         // car1.stop();
-        // car1.brake();
+        car1.brake();
+        car1.price();
 
         // Media carMedia = new Car();
         // carMedia.start();
         // carMedia.stop();
 
-        NiceCar car = new NiceCar();
-        car.start();
-        car.startMusic();
-        car.upgradeEngine();
-        car.start();
+        // NiceCar car = new NiceCar();
+        // car.start();
+        // car.startMusic();
+        // car.upgradeEngine();
+        // car.start();
+
+        PowerEngine car3 = new PowerEngine();
+        NiceCar car2 = new NiceCar(car3);
+        car2.start();
+        car2.stop();
+        car2.startMusic();
+        car2.upgradeEngine();
+        car2.start();
     }
 }
