e4bc5e137e9a6ba61bc6a1eb8f5f8e8cebaa9caf
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 14:22:24 +0530

    Create ElectricEngine.java

---
+++ interfacs/ElectricEngine.java
@@ -0,0 +1,20 @@
+package interfacs;
+
+public class ElectricEngine implements Engine{
+
+    @Override
+    public void start() {
+        System.out.println("electric engine start");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("electric engine stop");
+    }
+
+    @Override
+    public void acc() {
+        System.out.println("electric engine accelerates");
+    }
+    
+}
