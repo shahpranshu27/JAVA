b617609eef9788f928847a77f888f6b7ab337693
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 15:48:45 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,23 +1,23 @@
 class Bank5{
     float getRateOfInterest(){return 0;}
 }
-class SBI extends Bank5{
+class SBI2 extends Bank5{
     float getRateOfInterest(){return 6.7f;}
 }
-class AXIS extends Bank5{
+class AXIS2 extends Bank5{
     float getRateOfInterest(){return 7.7f;}
 }
-class ICICI extends Bank5{
+class ICICI2 extends Bank5{
     float getRateOfInterest(){return 5.7f;}
 }
 public class Polymorphism {
     public static void main(String[] args) {
         Bank5 b;
-        b = new SBI();
+        b = new SBI2();
         System.out.println("SBI : "+b.getRateOfInterest());
-        b = new AXIS();
+        b = new AXIS2();
         System.out.println("AXIS : "+b.getRateOfInterest());
-        b = new ICICI();
+        b = new ICICI2();
         System.out.println("ICICI : "+b.getRateOfInterest());
     }
 }
