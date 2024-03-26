1ec6feb06a9a2bcb8294a77825c5eaf4eacd5701
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 15:48:50 +0530

    Update SingleInheritance.java

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -3,12 +3,12 @@ class Animal1{
         System.out.println("Eating..!!");
     }
 }
-class Dog extends Animal1{
+class Dog4 extends Animal1{
     void bark(){System.out.println("Barking..!!");}
 }
 public class SingleInheritance {
     public static void main(String[] args) {
-        Dog d = new Dog();
+        Dog4 d = new Dog4();
         d.bark();
         d.eat();
     }
