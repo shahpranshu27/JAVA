85e78ae88de1ae12b40e722147ef384de1adbd4a
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 14:22:07 +0530

    Create PowerEngine.java

---
+++ interfacs/PowerEngine.java
@@ -0,0 +1,20 @@
+package interfacs;
+
+public class PowerEngine implements Engine{
+
+    @Override
+    public void start() {
+        System.out.println("power engine start");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("power engine stop");
+    }
+
+    @Override
+    public void acc() {
+        System.out.println("power engine accelerates");
+    }
+    
+}
