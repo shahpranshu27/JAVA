64f09a08ef8a87eace16fd2fa12c747e6120b61b
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 13:25:20 +0530

    Create Main.java

---
+++ interfacs/Main.java
@@ -0,0 +1,12 @@
+package interfacs;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+
+        car1.acc();
+        car1.start();
+        car1.stop();
+        car1.brake();
+    }
+}
