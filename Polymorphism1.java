8303d0e1b0bac6a1f66f8630297947d92b723ff7
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-26 15:48:47 +0530

    Update Polymorphism1.java

--- Polymorphism1.java
+++ Polymorphism1.java
@@ -1,23 +1,23 @@
 class Shape4{  
     void draw(){System.out.println("drawing...");}  
     }  
-    class Rectangle extends Shape4{  
+    class Rectangle2 extends Shape4{  
     void draw(){System.out.println("drawing rectangle...");}  
     }  
-    class Circle extends Shape4{  
+    class Circle2 extends Shape4{  
     void draw(){System.out.println("drawing circle...");}  
     }  
-    class Triangle extends Shape4{  
+    class Triangle2 extends Shape4{  
     void draw(){System.out.println("drawing triangle...");}  
 }  
 class Polymorphism1{  
     public static void main(String args[]){  
     Shape4 s;  
-    s=new Rectangle();  
+    s=new Rectangle2();  
     s.draw();  
-    s=new Circle();  
+    s=new Circle2();  
     s.draw();  
-    s=new Triangle();  
+    s=new Triangle2();  
     s.draw();  
     }  
 }
