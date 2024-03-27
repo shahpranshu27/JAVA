9098059dc075376c655c506aba770efd943eb59e
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-27 11:31:10 +0530

    Create CustomArrayList.java

---
+++ Generics/CustomArrayList.java
@@ -0,0 +1,79 @@
+// import java.util.ArrayList;
+
+import java.util.Arrays;
+
+public class CustomArrayList {
+
+    private int[] data;
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0; // also working as the index value
+
+    public CustomArrayList(){
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add(int num){
+        if(isFull()){
+            resize();
+        }
+        data[size++] = num;
+    }
+    
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    private void resize() {
+        int[] temp = new int[data.length * 2];
+
+        // copy the current items in new array
+
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
+    public int remove(){
+        int removed = data[--size];
+        return removed;
+    }
+
+    public int get(int index){
+        return data[index];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void set(int index, int value){
+        data[index] = value;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList [data=" + Arrays.toString(data) + ", size=" + size + "]";
+    }
+
+    public static void main(String[] args) {
+        // ArrayList list = new ArrayList<>();
+        // list.add(45);
+        // list.remove(0);
+        // list.get(0);
+        // list.set(1,234);
+        // list.size();
+        // list.isEmpty();
+
+         CustomArrayList list = new CustomArrayList();
+        //  list.add(3);
+        //  list.add(5);
+        //  list.add(4);
+
+        for (int i = 0; i < 14; i++) {
+            list.add(2 * i);
+        }
+         
+        System.out.println(list);
+    }
+}
