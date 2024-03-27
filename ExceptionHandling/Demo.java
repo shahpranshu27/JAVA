f4e72960a319879561a6a40455a4a334f807f349
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-03-27 15:50:19 +0530

    Create Demo.java

---
+++ ExceptionHandling/Demo.java
@@ -0,0 +1,7 @@
+package ExceptionHandling;
+
+public class Demo {
+    public static void main(String[] args) {
+        Main.divide(3, 0); // here, it will throw the same error : "please don't divide by 0"
+    }
+}
