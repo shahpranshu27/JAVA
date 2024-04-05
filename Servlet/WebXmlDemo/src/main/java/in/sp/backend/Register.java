7a231de5dfdfae3be75144c1ddcea3a3b789b18b
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-05 16:14:26 +0530

    Added WebXmlDemo folder

---
+++ Servlet/WebXmlDemo/src/main/java/in/sp/backend/Register.java
@@ -0,0 +1,19 @@
+package in.sp.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("I am in Register class :) Yayy");
+		System.out.println("I am in Register class!!");
+	}
+}
