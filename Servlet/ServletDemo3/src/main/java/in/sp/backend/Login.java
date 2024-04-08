63e7988230f5bf7ac81b90a2b15322bac35e9c3b
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-08 12:40:04 +0530

    Added ServletDemo3 ( RequestDispatcher )

---
+++ Servlet/ServletDemo3/src/main/java/in/sp/backend/Login.java
@@ -0,0 +1,37 @@
+package in.sp.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class Login extends HttpServlet
+{
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String myEmail = request.getParameter("email1");
+		String myPass =  request.getParameter("pass1");
+		
+		PrintWriter out = response.getWriter();
+		
+		if(myEmail.equals("abc@gmail.com") && myPass.equals("1234")) {
+//			Object pranshu = request.getAttribute("name_key");
+			request.getAttribute("name_key");
+			RequestDispatcher rd =  request.getRequestDispatcher("/profile.jsp");
+			rd.forward(request, response);
+		}
+		else {
+			response.setContentType("text/html");
+			out.println("<h3 style='color:red'>The email and password you entered are incorrect!</h3>");
+			RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+//			rd.forward(request, response);
+			rd.include(request, response);
+		}
+	}
+}
