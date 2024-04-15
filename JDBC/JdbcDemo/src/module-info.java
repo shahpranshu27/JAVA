784054c4d7d559dd9981752c39c5e8f91335289e
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-15 14:12:56 +0530

    jdbc demo

---
+++ JDBC/JdbcDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JdbcDemo {
+	requires java.sql;
+}
---
+++ JDBC/JdbcDemo/src/myPackage/Main.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student st = new Student();
+//		st.createDatabase(); // since i have already created a database here
+		
+//		st.createTable();
+		
+//		st.createData();
+		
+		st.readData();
+	}
+
+}
